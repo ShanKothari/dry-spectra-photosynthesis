7c0a97025d7a700ee27766766d5fe660af3a2a80
Shan Kothari <quercusacerifolia@gmail.com>
2024-12-17 21:54:07 -0500

    add dry models

---
+++ 03B dry_models.R
@@ -0,0 +1,280 @@
+setwd("C:/Users/Shan Kothari/Dropbox/PostdocProjects/PosadaSpectra/")
+
+library(spectrolab)
+library(caret)
+library(pls)
+
+dry_spectra<-readRDS("ProcessedData/dry_spectra_and_traits.rds")
+dry_spectra_norm<-normalize(dry_spectra)
+
+###########################################################################
+## non-normalized
+
+## split training and testing data
+train_sample <- createDataPartition(
+  y = meta(dry_spectra)$Species,
+  p = .6,
+  list = FALSE
+)
+
+spectra_train<-dry_spectra[train_sample]
+spectra_test<-dry_spectra[-train_sample]
+
+## LDMC
+LDMC_model<-plsr(meta(spectra_train)$LDMC~as.matrix(spectra_train),
+                 ncomp=30,method = "oscorespls",validation="CV",segments=10)
+
+ncomp_LDMC <- selectNcomp(LDMC_model, method = "onesigma", plot = FALSE)
+
+LDMC_val_pred<-data.frame(sample=meta(spectra_test)$sample_id,
+                          val_pred=predict(LDMC_model,newdata=as.matrix(spectra_test),ncomp=ncomp_LDMC)[,,1],
+                          measured=meta(spectra_test)$LDMC)
+
+ggplot(LDMC_val_pred,aes(y=measured,x=val_pred))+
+  geom_point(size=2)+geom_smooth(method="lm",se=F)+
+  theme_bw()+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  coord_cartesian(xlim=c(0.20,0.5),ylim=c(0.20,0.5))+
+  theme(text = element_text(size=20),
+        legend.position = c(0.8, 0.2))+
+  labs(y="Measured",x="Predicted")+
+  ggtitle("Predicting LDMC")
+
+## LMA
+LMA_model<-plsr(meta(spectra_train)$LMA~as.matrix(spectra_train),
+                ncomp=30,method = "oscorespls",validation="CV",segments=10)
+
+ncomp_LMA <- selectNcomp(LMA_model, method = "onesigma", plot = FALSE)
+
+LMA_val_pred<-data.frame(sample=meta(spectra_test)$sample_id,
+                         val_pred=predict(LMA_model,newdata=as.matrix(spectra_test),ncomp=ncomp_LMA)[,,1],
+                         measured=meta(spectra_test)$LMA)
+
+ggplot(LMA_val_pred,aes(y=measured,x=val_pred))+
+  geom_point(size=2)+geom_smooth(method="lm",se=F)+
+  theme_bw()+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  theme(text = element_text(size=20),
+        legend.position = c(0.8, 0.2))+
+  labs(y="Measured",x="Predicted")+
+  ggtitle("Predicting LMA")
+
+## Asat
+Asat_model<-plsr(meta(spectra_train)$Asat~as.matrix(spectra_train),
+                 ncomp=30,method = "oscorespls",validation="CV",segments=10)
+
+ncomp_Asat <- selectNcomp(Asat_model, method = "onesigma", plot = FALSE)
+
+Asat_val_pred<-data.frame(sample=meta(spectra_test)$sample_id,
+                          val_pred=predict(Asat_model,newdata=as.matrix(spectra_test),ncomp=ncomp_Asat)[,,1],
+                          measured=meta(spectra_test)$Asat)
+
+ggplot(Asat_val_pred,aes(y=measured,x=val_pred))+
+  geom_point(size=2)+geom_smooth(method="lm",se=F)+
+  theme_bw()+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  theme(text = element_text(size=20),
+        legend.position = c(0.8, 0.2))+
+  labs(y="Measured",x="Predicted")+
+  ggtitle("Predicting Asat")
+
+## Vcmax25
+Vcmax25_model<-plsr(meta(spectra_train)$Vcmax25~as.matrix(spectra_train),
+                 ncomp=30,method = "oscorespls",validation="CV",segments=10)
+
+ncomp_Vcmax25 <- selectNcomp(Vcmax25_model, method = "onesigma", plot = FALSE)
+
+Vcmax25_val_pred<-data.frame(sample=meta(spectra_test)$sample_id,
+                          val_pred=predict(Vcmax25_model,newdata=as.matrix(spectra_test),ncomp=ncomp_Vcmax25)[,,1],
+                          measured=meta(spectra_test)$Vcmax25)
+
+ggplot(Vcmax25_val_pred,aes(y=measured,x=val_pred))+
+  geom_point(size=2)+geom_smooth(method="lm",se=F)+
+  theme_bw()+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  theme(text = element_text(size=20),
+        legend.position = c(0.8, 0.2))+
+  labs(y="Measured",x="Predicted")+
+  ggtitle("Predicting Vcmax25")
+
+###########################################################################
+## non-normalized, without conifers
+
+dry_spectra_nc<-dry_spectra[!meta(dry_spectra)$leaf_type=="needleleaf",]
+
+## split training and testing data
+train_sample <- createDataPartition(
+  y = meta(dry_spectra_nc)$Species,
+  p = .6,
+  list = FALSE
+)
+
+spectra_train<-dry_spectra_nc[train_sample]
+spectra_test<-dry_spectra_nc[-train_sample]
+
+## LDMC
+LDMC_model<-plsr(meta(spectra_train)$LDMC~as.matrix(spectra_train),
+                 ncomp=30,method = "oscorespls",validation="CV",segments=10)
+
+ncomp_LDMC <- selectNcomp(LDMC_model, method = "onesigma", plot = FALSE)
+
+LDMC_val_pred<-data.frame(sample=meta(spectra_test)$sample_id,
+                          val_pred=predict(LDMC_model,newdata=as.matrix(spectra_test),ncomp=ncomp_LDMC)[,,1],
+                          measured=meta(spectra_test)$LDMC)
+
+ggplot(LDMC_val_pred,aes(y=measured,x=val_pred))+
+  geom_point(size=2)+geom_smooth(method="lm",se=F)+
+  theme_bw()+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  coord_cartesian(xlim=c(0.20,0.5),ylim=c(0.20,0.5))+
+  theme(text = element_text(size=20),
+        legend.position = c(0.8, 0.2))+
+  labs(y="Measured",x="Predicted")+
+  ggtitle("Predicting LDMC")
+
+## LMA
+LMA_model<-plsr(meta(spectra_train)$LMA~as.matrix(spectra_train),
+                ncomp=30,method = "oscorespls",validation="CV",segments=10)
+
+ncomp_LMA <- selectNcomp(LMA_model, method = "onesigma", plot = FALSE)
+
+LMA_val_pred<-data.frame(sample=meta(spectra_test)$sample_id,
+                         val_pred=predict(LMA_model,newdata=as.matrix(spectra_test),ncomp=ncomp_LMA)[,,1],
+                         measured=meta(spectra_test)$LMA)
+
+ggplot(LMA_val_pred,aes(y=measured,x=val_pred))+
+  geom_point(size=2)+geom_smooth(method="lm",se=F)+
+  theme_bw()+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  theme(text = element_text(size=20),
+        legend.position = c(0.8, 0.2))+
+  labs(y="Measured",x="Predicted")+
+  ggtitle("Predicting LMA")
+
+## Asat
+Asat_model<-plsr(meta(spectra_train)$Asat~as.matrix(spectra_train),
+                 ncomp=30,method = "oscorespls",validation="CV",segments=10)
+
+ncomp_Asat <- selectNcomp(Asat_model, method = "onesigma", plot = FALSE)
+
+Asat_val_pred<-data.frame(sample=meta(spectra_test)$sample_id,
+                          val_pred=predict(Asat_model,newdata=as.matrix(spectra_test),ncomp=ncomp_Asat)[,,1],
+                          measured=meta(spectra_test)$Asat)
+
+ggplot(Asat_val_pred,aes(y=measured,x=val_pred))+
+  geom_point(size=2)+geom_smooth(method="lm",se=F)+
+  theme_bw()+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  theme(text = element_text(size=20),
+        legend.position = c(0.8, 0.2))+
+  labs(y="Measured",x="Predicted")+
+  ggtitle("Predicting Asat")
+
+## Vcmax25
+Vcmax25_model<-plsr(meta(spectra_train)$Vcmax25~as.matrix(spectra_train),
+                    ncomp=30,method = "oscorespls",validation="CV",segments=10)
+
+ncomp_Vcmax25 <- selectNcomp(Vcmax25_model, method = "onesigma", plot = FALSE)
+
+Vcmax25_val_pred<-data.frame(sample=meta(spectra_test)$sample_id,
+                             val_pred=predict(Vcmax25_model,newdata=as.matrix(spectra_test),ncomp=ncomp_Vcmax25)[,,1],
+                             measured=meta(spectra_test)$Vcmax25)
+
+ggplot(Vcmax25_val_pred,aes(y=measured,x=val_pred))+
+  geom_point(size=2)+geom_smooth(method="lm",se=F)+
+  theme_bw()+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  theme(text = element_text(size=20),
+        legend.position = c(0.8, 0.2))+
+  labs(y="Measured",x="Predicted")+
+  ggtitle("Predicting Vcmax25")
+
+#########################
+## brightness-normalized
+
+## split training and testing data
+train_sample <- createDataPartition(
+  y = meta(dry_spectra_norm)$Species,
+  p = .6,
+  list = FALSE
+)
+
+spectra_train<-dry_spectra_norm[train_sample]
+spectra_test<-dry_spectra_norm[-train_sample]
+
+## LDMC
+LDMC_model<-plsr(meta(spectra_train)$LDMC~as.matrix(spectra_train),
+                 ncomp=30,method = "oscorespls",validation="CV",segments=10)
+
+ncomp_LDMC <- selectNcomp(LDMC_model, method = "onesigma", plot = FALSE)
+
+LDMC_val_pred<-data.frame(sample=meta(spectra_test)$sample_id,
+                          val_pred=predict(LDMC_model,newdata=as.matrix(spectra_test),ncomp=ncomp_LDMC)[,,1],
+                          measured=meta(spectra_test)$LDMC)
+
+ggplot(LDMC_val_pred,aes(y=measured,x=val_pred))+
+  geom_point(size=2)+geom_smooth(method="lm",se=F)+
+  theme_bw()+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  coord_cartesian(xlim=c(0.20,0.5),ylim=c(0.20,0.5))+
+  theme(text = element_text(size=20),
+        legend.position = c(0.8, 0.2))+
+  labs(y="Measured",x="Predicted")+
+  ggtitle("Predicting LDMC")
+
+## LMA
+LMA_model<-plsr(meta(spectra_train)$LMA~as.matrix(spectra_train),
+                ncomp=30,method = "oscorespls",validation="CV",segments=10)
+
+ncomp_LMA <- selectNcomp(LMA_model, method = "onesigma", plot = FALSE)
+
+LMA_val_pred<-data.frame(sample=meta(spectra_test)$sample_id,
+                         val_pred=predict(LMA_model,newdata=as.matrix(spectra_test),ncomp=ncomp_LMA)[,,1],
+                         measured=meta(spectra_test)$LMA)
+
+ggplot(LMA_val_pred,aes(y=measured,x=val_pred))+
+  geom_point(size=2)+geom_smooth(method="lm",se=F)+
+  theme_bw()+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  theme(text = element_text(size=20),
+        legend.position = c(0.8, 0.2))+
+  labs(y="Measured",x="Predicted")+
+  ggtitle("Predicting LMA")
+
+## Asat
+Asat_model<-plsr(meta(spectra_train)$Asat~as.matrix(spectra_train),
+                 ncomp=30,method = "oscorespls",validation="CV",segments=10)
+
+ncomp_Asat <- selectNcomp(Asat_model, method = "onesigma", plot = FALSE)
+
+Asat_val_pred<-data.frame(sample=meta(spectra_test)$sample_id,
+                          val_pred=predict(Asat_model,newdata=as.matrix(spectra_test),ncomp=ncomp_Asat)[,,1],
+                          measured=meta(spectra_test)$Asat)
+
+ggplot(Asat_val_pred,aes(y=measured,x=val_pred))+
+  geom_point(size=2)+geom_smooth(method="lm",se=F)+
+  theme_bw()+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  theme(text = element_text(size=20),
+        legend.position = c(0.8, 0.2))+
+  labs(y="Measured",x="Predicted")+
+  ggtitle("Predicting Asat")
+
+## Vcmax25
+Vcmax25_model<-plsr(meta(spectra_train)$Vcmax25~as.matrix(spectra_train),
+                 ncomp=30,method = "oscorespls",validation="CV",segments=10)
+
+ncomp_Vcmax25 <- selectNcomp(Vcmax25_model, method = "onesigma", plot = FALSE)
+
+Vcmax25_val_pred<-data.frame(sample=meta(spectra_test)$sample_id,
+                          val_pred=predict(Vcmax25_model,newdata=as.matrix(spectra_test),ncomp=ncomp_Vcmax25)[,,1],
+                          measured=meta(spectra_test)$Vcmax25)
+
+ggplot(Vcmax25_val_pred,aes(y=measured,x=val_pred))+
+  geom_point(size=2)+geom_smooth(method="lm",se=F)+
+  theme_bw()+
+  geom_abline(slope=1,intercept=0,linetype="dashed",size=2)+
+  theme(text = element_text(size=20),
+        legend.position = c(0.8, 0.2))+
+  labs(y="Measured",x="Predicted")+
+  ggtitle("Predicting Vcmax25")
